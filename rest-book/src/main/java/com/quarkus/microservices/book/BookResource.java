d0090106c5cfc8c1fbdae3b70b1e28258d07d8ef
Neimat1 <neimat.soliman.ismail@gmail.com>
2024-09-01 14:08:26 +0300

    rest-book: Exposing book Rest endpoint

--- rest-book/src/main/java/com/quarkus/microservices/book/BookResource.java
+++ rest-book/src/main/java/com/quarkus/microservices/book/BookResource.java
@@ -1,16 +1,35 @@
 package com.quarkus.microservices.book;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.jboss.logging.Logger;
+
+import java.time.Instant;
 
 @Path("/api/books")
 public class BookResource {
 
-    @GET
+    @Inject
+    Logger logger;
+
+    @POST
     @Produces(MediaType.APPLICATION_JSON)
-    public String getBook(){
-        return "new book";
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    public Response createABook(@FormParam("title") String title,
+                                @FormParam("author") String author,
+                                @FormParam("yearOfPublication") int yearOfPublication,
+                                @FormParam("genre") String genre) {
+        Book book = new Book();
+        book.isbn13 = "We will get it later from the Number Microservice";
+        book.title = title;
+        book.author = author;
+        book.yearOfPublication = yearOfPublication;
+        book.genre = genre;
+        book.creationDate = Instant.now();
+
+        logger.info("Book created: "+ book);
+        return Response.ok(201).entity(book).build();
     }
 }
